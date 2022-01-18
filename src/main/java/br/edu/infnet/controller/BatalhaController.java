85855897107de47934d955e0493bf4f832a1edb2
Anderson Fagundes <anderson.fagundes4@gmail.com>
2022-01-18 17:29:56 -0300

    Implementado método de inicializar a batalha

--- src/main/java/br/edu/infnet/controller/BatalhaController.java
+++ src/main/java/br/edu/infnet/controller/BatalhaController.java
@@ -1,9 +1,14 @@
 package br.edu.infnet.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,32 +19,63 @@ import com.google.gson.Gson;
 public class BatalhaController {
 	
 	@Autowired
-	RestTemplate restTemplate;
-
-//    @GetMapping
-//    public void teste(){
-//        RestTemplate restTemplate = new RestTemplate();
-//        String forObject = restTemplate.getForObject("http://localhost:8080/api/rolldices/iniciativa", String.class);
-//        
-//        Gson gson = new Gson();
-//
-//    	String json = gson.toJson(forObject);
-//    	
-//        System.out.println(json);
-//    }
-//    
-    @GetMapping
-    public ResponseEntity getResultDices() {
+	RestTemplate restTemplate; 
+    
+    @GetMapping(value = "/iniciativa", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Map<String, Object>> getStart(@RequestParam(name = "idHero") Integer idHero,
+																@RequestParam(name = "agilityHero") Integer agilityHero,
+																@RequestParam(name = "idMonster") Integer idMonster,
+																@RequestParam(name = "agilityMonster") Integer agilityMonster) {
+    	
+    	int type = 10;
+    	int amount = 1;
+    	
     	try {
-    		//RestTemplate restTemplate = new RestTemplate();
-    		String forObject = restTemplate.getForObject("http://DICE-API/api/rolldices/iniciativa", String.class);
-        
-    		Gson gson = new Gson();
-        
-    		String json = gson.toJson(forObject);
-    		return ResponseEntity.ok(json);
+    		
+    		int resultDice = 0;
+	    	int resultHero = 0;
+	    	int resultMonster = 0;
+	    	boolean hasResult = false;
+
+	    	do {
+    			
+    			resultDice = getResultRollDices(type, amount);
+    			
+    			resultHero = resultDice + agilityHero;
+    			resultMonster = resultDice + agilityMonster;
+
+    			if ((resultHero > resultMonster) || (resultMonster > resultHero)) hasResult = true;
+
+			} while (!hasResult);
+    		    		
+    		String resultDices = "1";
+    		
+    		Map<String, Object> payload = new HashMap<>();
+    		payload.put("value", resultDices);
+    		
+    		return ResponseEntity.ok(payload);
     	} catch(IllegalArgumentException ex) {
     		return (ResponseEntity) ResponseEntity.internalServerError();
     	}
+		
 	}
+    
+    public Integer getResultRollDices(Integer type, Integer amount) {
+    	
+    	//RestTemplate restTemplate = new RestTemplate();
+		String forObject = restTemplate.getForObject("http://DICE-API/api/rolldices?type="+type+"&amount="+amount, String.class);
+		
+		Gson gson = new Gson();
+    
+		String resultDices = gson.toJson(forObject);
+		int result;
+		
+		if(resultDices.substring(13,14).equals("}")) {
+			result = Integer.parseInt(resultDices.substring(12,13));
+		} else{
+			result = Integer.parseInt(resultDices.substring(12,14));
+		} 
+    	
+    	return result;
+    }
 }
